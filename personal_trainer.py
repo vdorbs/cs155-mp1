5a1bf25cc368925b5c52014684f4fe305449ff72
Victor Dorobantu <victor.d.dorobantu@gmail.com>
2018-02-03 12:55:38 -0800

    Added basic SVM

--- personal_trainer.py
+++ personal_trainer.py
@@ -1,12 +1,15 @@
 import sys
 import numpy as np
+from sklearn.linear_model import SGDClassifier
 
 def personal_trainer(path):
     data = np.load(path)
     print(data.shape)
     x, y = data[:,1:], data[:,1]
-    print(x.shape)
-    print(y.shape)
+    model = SGDClassifier()
+    model.fit(x, y)
+    y_hat = model.predict(x)
+    print(np.linalg.norm(y - y_hat, 1) / len(y))
 
 if __name__ == '__main__':
     try:
