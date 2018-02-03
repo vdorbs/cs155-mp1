ec879e3cd893af73f8ec40f00497bbf2e43faf7b
Victor Dorobantu <victor.d.dorobantu@gmail.com>
2018-02-03 13:01:32 -0800

    Added DecisionTreeClassifier

--- personal_trainer.py
+++ personal_trainer.py
@@ -1,12 +1,13 @@
 import sys
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import SGDClassifier
 
 def personal_trainer(path):
     data = np.load(path)
     print(data.shape)
     x, y = data[:,1:], data[:,1]
-    model = SGDClassifier()
+    model = DecisionTreeClassifier()
     model.fit(x, y)
     y_hat = model.predict(x)
     print(np.linalg.norm(y - y_hat, 1) / len(y))
