d81ca6f4488cd6c0041e5d3e0e14b99ff4217332
Kristján Eldjárn <kristjan@eldjarn.net>
2018-02-03 12:59:39 -0800

    Created a method for calculating the tf_idf of a histogram matrix

--- personal_trainer.py
+++ personal_trainer.py
@@ -1,12 +1,18 @@
 import sys
 import numpy as np
 
+def tf_idf(x):
+    idf = np.log(x.shape[0] / np.apply_along_axis(np.count_nonzero, 0, x))
+    tf = (x.T/x.sum()).T
+    return tf * idf
+
 def personal_trainer(path):
     data = np.load(path)
     print(data.shape)
     x, y = data[:,1:], data[:,1]
     print(x.shape)
     print(y.shape)
+    tf_idf(x)
 
 if __name__ == '__main__':
     try:
