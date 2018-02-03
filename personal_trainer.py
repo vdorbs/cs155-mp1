abe8dfd9589fcca9935950826f4128cd3165729b
Kristján Eldjárn <kristjan@eldjarn.net>
2018-02-03 15:16:43 -0800

    Created Test class

--- personal_trainer.py
+++ personal_trainer.py
@@ -2,20 +2,34 @@ import sys
 import numpy as np
 from sklearn.linear_model import SGDClassifier
 
+class Test:
+    def __init__(self, name, model):
+        self.name = name
+        self.model = model
+
+    def fit(self, x, y):
+        self.model.fit(x ,y)
+
+    def score(self):
+        return self.model.score
+
+
+
 def tf_idf(x):
     idf = np.log(x.shape[0] / np.apply_along_axis(np.count_nonzero, 0, x))
     tf = (x.T / np.sum(x.T + np.finfo(float).eps, 0)).T
-    return tf * idf
+    tf_idf = tf * idf
+    print(np.array_equiv(np.where(tf_idf != 0), np.where(x != 0)))
 
 def personal_trainer(path):
     data = np.load(path)
     print(data.shape)
     x, y = data[:,1:], data[:,1]
     x_normal = tf_idf(x)
-    model = SGDClassifier()
-    model.fit(x, y)
-    y_hat = model.predict(x)
-    print(np.linalg.norm(y - y_hat, 1) / len(y))
+    # model = SGDClassifier()
+    # model.fit(x, y)
+    # y_hat = model.predict(x)
+    # print(np.linalg.norm(y - y_hat, 1) / len(y))
 
 if __name__ == '__main__':
     try:
